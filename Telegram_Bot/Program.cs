76b04c1d1f65d24b930eaaf6f1cb6be7ecf7575e
Nikitkaaav <144979489+Nikitkaaav@users.noreply.github.com>
2024-05-19 16:31:04 +0500

    TG Bot

---
+++ Telegram_Bot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host bot = new Host("7038593299:AAEkEuD54turzP9lr_xHzqqWSx8-yhAeLac");
+            bot.Start();
+            bot.OnMessage += OnMessage;
+            Console.ReadLine();
+        }
+
+        private static async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 0, update.Message?.Text ?? "[не текст]");
+        }
+    }
+}
